0cb7e3cb7a7f5412abf7ca3795a13c9bb43288ca
TonyArPe <tonyaragonp@gmail.com>
2024-12-06 12:11:10 +0100

    Modificacion de professor. Contactar por email.

--- app/src/main/java/com/example/loginycardview/Professor.kt
+++ app/src/main/java/com/example/loginycardview/Professor.kt
@@ -1,8 +1,8 @@
-package com.example.loginycardview
-
 data class Professor(
     val imageResId: Int,
     val name: String,
     val specialty: String,
-    val isTopRated: Boolean
+    val isTopRated: Boolean,
+    val description: String,
+    val email: String
 )
